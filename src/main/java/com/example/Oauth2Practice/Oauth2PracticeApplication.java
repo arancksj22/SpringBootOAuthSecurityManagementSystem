3f4b3f46654e95feea698f952fe70a54a1c85dc3
arancksj22 <arancksj@gmail.com>
2025-11-16 21:29:44 +0530

    skeleton

---
+++ src/main/java/com/example/Oauth2Practice/Oauth2PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Oauth2Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2PracticeApplication.class, args);
+	}
+
+}
