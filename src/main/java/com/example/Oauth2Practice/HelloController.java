6dcc1713ea6ec6a01802108ad85b82bd6078271c
arancksj22 <arancksj@gmail.com>
2025-11-16 21:32:01 +0530

    security file

--- src/main/java/com/example/Oauth2Practice/HelloController.java
+++ src/main/java/com/example/Oauth2Practice/HelloController.java
@@ -1,10 +1,12 @@
 package com.example.Oauth2Practice;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @GetMapping("/")
     public String greet(){
         return "Hello, aranck this is working!";
     }
